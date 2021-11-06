811869c82c0d489205a9a202ecb764de93994057
marcuspousette <marcuspousette@gmail.com>
2021-11-06 12:40:31 +0100

    init

--- src/loaders/loaders.js
+++ src/loaders/loaders.js
@@ -1,35 +1,37 @@
-import config from '../config';
+import config from "../config";
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const handleBars = require("./express-handlebars");
 
-app.use(require('./express-handlebars'));
-app.use('/static', express.static('public'));
+app.use(handleBars);
+app.use("/static", express.static("public"));
+
 // Set caching and assets
-if (config.node_env === 'production') {
-    app.enable('view cache');
+if (config.node_env === "production") {
+  app.enable("view cache");
 }
 
+// Import our routes:
+const index = require("../api/client/index");
+const confirmation = require("../api/client/confirmation");
+const push = require("../api/client/push");
+const notFound = require("../api/both/404.js");
+const serviceUnavailable = require("../api/both/500.js");
 
-// Routes:
-const index = require('../api/client/index');
-const confirmation = require('../api/client/confirmation');
-const push = require('../api/client/push');
-const notFound = require('../api/both/404.js');
-const serviceUnavailable = require('../api/both/500.js');
-// Client Routes
-app.use('/', index);
-app.use('/confirmation', confirmation);
-app.use('/push', push);
+// Use our Client Routes
+app.use("/", index);
+app.use("/confirmation", confirmation);
+app.use("/push", push);
 
 // Server Routes
 
 // Error Pages:
-app.use('/500', serviceUnavailable);
-app.use('*', notFound); // Always keep as last route
+app.use("/500", serviceUnavailable);
+app.use("*", notFound); // Always keep as last route
 
 app.listen(config.port, function () {
-    console.log(`App listening on port ${config.port}`);
+  console.log(`App listening on port ${config.port}`);
 });
 
 module.exports = app;
